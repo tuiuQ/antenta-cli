5ceb752b96b8499548a43ef8388a8361f4509221
tuiu <13719283454@163.com>
2021-07-16 03:03:42 +0800

    完成npmlog简单封装

---
+++ packages/@antenta/cli-utils/lib/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.log = void 0;
+var log_1 = require("./log");
+Object.defineProperty(exports, "log", { enumerable: true, get: function () { return __importDefault(log_1).default; } });
