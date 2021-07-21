df4b0ea47bb33f51ba98c8907e49e79124068367
tuiu <13719283454@163.com>
2021-07-22 05:03:59 +0800

    gulp

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,1 @@
-export declare function an(): void;
+
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { core, name } from "./core"
-
-export function an (): void {
-  core(name)
-  core('Antenta UI')
-  core('windows')
-  core('就这')
-}
+console.log('hello')
